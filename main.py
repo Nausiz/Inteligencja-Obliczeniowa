5fd377a7d94b6a2c3793cffb2ea3af125edc6d6e
Michał Mazur <michal.mazur4@edu.eukat.pl>
2022-03-06 17:48:43 +0100

    Przyklad generacji punktów na mapie

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-print("Test")
-print("Test2")
-print("Test3")
-print("Test4")
+#print("Test")
+#print("Test2")
+#print("Test3")
+#print("Test4")
+
+#przykładowa generacja miejsc na mapie
+import random
+import numpy as np
+
+mapa = np.zeros((100, 100))
+n = random.randint(400, 600)
+for i in range(n):
+    a = random.randint(0, 9999)
+    b = int(a/100)
+    c = a % 100
+    mapa[b][c] = 1
+print(mapa)
