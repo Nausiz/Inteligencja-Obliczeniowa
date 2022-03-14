a4e43587afb9439fed4ad03a37bac321296a1b95
theLahcim <michal.mazur32@gmail.com>
2022-03-14 15:35:33 +0100

    rysowanie mapy

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # przykładowa generacja miejsc na mapie
 from mapa import map_gener, magazyn_kord, dostawa_kord
+import matplotlib.pyplot as plt
+import pandas as pd
 
 mapa = map_gener()
 magazyn = magazyn_kord(mapa)
 dostawa = dostawa_kord(mapa)
-print(mapa)
+# print(mapa)
 print(f'Magazyny: {magazyn}')
 print(f'Dostawy: {dostawa}')
+
+mg = pd.DataFrame(magazyn, columns=['x', 'y'])
+dst = pd.DataFrame(dostawa, columns=['x', 'y'])
+# print(mg)
+plt.plot(mg['x'], mg['y'], '.g')
+plt.plot(dst['x'], dst['y'], '.y')
+plt.show()
--- mapa.py
+++ mapa.py
@@ -1,41 +1,40 @@
 import random
-import numpy as np
 
 
 # generacja mapy 100 na 100
 def map_gener():
-    mapa = np.zeros((100, 100))
+    mapa = []
+    for i in range(10000):
+        b = int(i / 100)
+        c = i % 100
+        mapa.append([b, c, ""])
     # generacja n punktów dostawy
     n = random.randint(400, 600)
     # print(f'n:{n}')
     points = random.sample(range(10000), n)
     for i in range(n-5):
         a = points[i]
-        b = int(a/100)
-        c = a % 100
-        mapa[b][c] = 1
+        mapa[a][2] = "dostawa"
     # generacja magazynu
     for i in range(5):
         a = points[n-5+i]
-        b = int(a/100)
-        c = a % 100
-        mapa[b][c] = 2
+        mapa[a][2] = "magazyn"
     return mapa
 
 
 # koordynaty magazynu
 def magazyn_kord(mapa):
-    result = np.where(mapa == 2)
+    result = [i for i, x in enumerate(mapa) if x[2] == "magazyn"]
     magazyn = []
     for i in range(5):
-        magazyn.append([int(result[0][i]), int(result[1][i])])
+        magazyn.append([mapa[int(result[i])][0], mapa[int(result[i])][1]])
     return magazyn
 
 
 # koordynaty dostaw
 def dostawa_kord(mapa):
-    result = np.where(mapa == 1)
+    result = [i for i, x in enumerate(mapa) if x[2] == "dostawa"]
     dostawa = []
-    for i in range(len(result[0])):
-        dostawa.append([int(result[0][i]), int(result[1][i])])
+    for i in range(len(result)):
+        dostawa.append([mapa[int(result[i])][0], mapa[int(result[i])][1]])
     return dostawa
