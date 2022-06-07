43b787cbe66f66fbd380ad3b3a6f1460b31b7ca9
theLahcim <michal.mazur32@gmail.com>
2022-06-07 16:59:06 +0200

    trasa i poprawki

--- Klasy/utils.py
+++ Klasy/utils.py
@@ -10,14 +10,32 @@ def najblizszy_punkt(samochod: Samochod, lista_punktow: list) -> list:
         dlugosc = math.sqrt((row[0]-samochod.pozycja[0])**2+(row[1]-samochod.pozycja[1])**2)
         if dlugosc < min_dlugosc:
             czy_odbior = (row[3] == 'Odbierz')
-            zaladunek_po_odbiorze = row[2] + samochod.aktualny_zaladunek
+            if czy_odbior:
+                zaladunek_po_odbiorze = row[2] + samochod.aktualny_zaladunek
+            else:
+                zaladunek_po_odbiorze = samochod.aktualny_zaladunek - row[2]
             if czy_odbior and zaladunek_po_odbiorze <= samochod.pojemnosc:
                 min_dlugosc = dlugosc
                 min_x = row[0]
                 min_y = row[1]
-            elif not czy_odbior:
+            elif not czy_odbior and zaladunek_po_odbiorze > 0:
                 min_dlugosc = dlugosc
                 min_x = row[0]
                 min_y = row[1]
+    if min_dlugosc < 200:
+        samochod.pozycja = [min_x, min_y]
+    return [min_x, min_y]
 
+
+def najblizszy_magazyn(samochod: Samochod, lista_punktow: list) -> list:
+    min_dlugosc = 200
+    min_x = -1
+    min_y = -1
+    for row in lista_punktow:
+        dlugosc = math.sqrt((row[0] - samochod.pozycja[0]) ** 2 + (row[1] - samochod.pozycja[1]) ** 2)
+        if dlugosc < min_dlugosc:
+            min_dlugosc = dlugosc
+            min_x = row[0]
+            min_y = row[1]
+    samochod.pozycja = [min_x, min_y]
     return [min_x, min_y]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # przykładowa generacja miejsc na mapie
 from mapa import map_gener, magazyn_kord, dostawa_kord
 from samochody import losuj_samochody
-from Klasy.utils import najblizszy_punkt
+from Klasy.utils import najblizszy_punkt, najblizszy_magazyn
 import plotly.express as px
 import pandas as pd
 
@@ -21,8 +21,50 @@ mg = pd.DataFrame(magazyn, columns=['x', 'y'])
 dst = pd.DataFrame(dostawa, columns=['x', 'y', 'wielkosc', 'dostarcz/odbierz'])
 print(dst)
 
-print(najblizszy_punkt(samochody[0], dostawa))
+#print(najblizszy_punkt(samochody[0], dostawa))
 
 fig = px.scatter(dst, x='x', y='y', color=dst['dostarcz/odbierz'])
-fig.add_scatter(x=mg['x'], y=mg['y'], mode="markers")
+fig.add_scatter(x=mg['x'], y=mg['y'], mode="markers", name='magazyn')
+
+# d = {'x': [0, 1, 2, 3], 'y': [0, 1, 2, 3]}
+# df1 = pd.DataFrame(data=d)
+# d = {'x': [32, 12, 11, 13], 'y': [0, 1, 2, 3]}
+# df2 = pd.DataFrame(data=d)
+
+# tab = [df1, df2]
+trasy = []
+for s in samochody:
+    trasy.append({'x': [s.pozycja[0]], 'y': [s.pozycja[1]]})
+# trasa = {'x': [], 'y': []}
+ile = 0
+while True:
+    for i, s in enumerate(samochody):
+        p = najblizszy_punkt(s, dostawa)
+        dostarczony = [j for j, x in enumerate(dostawa) if x[0] == p[0] and x[1] == p[1]]
+        if len(dostarczony) > 0:
+            if dostawa[dostarczony[0]][3] == 'Odbierz':
+                s.aktualny_zaladunek = s.aktualny_zaladunek + dostawa[dostarczony[0]][2]
+            else:
+                s.aktualny_zaladunek = s.aktualny_zaladunek - dostawa[dostarczony[0]][2]
+        else:
+            p = najblizszy_magazyn(s, magazyn)
+            s.aktualny_zaladunek = s.pojemnosc * 0.8
+        if s.aktualny_zaladunek < 0:
+            print(f'zjebało się {s.aktualny_zaladunek}')
+        dostawa.pop(dostarczony[0])
+        trasy[i]['x'].append(p[0])
+        trasy[i]['y'].append(p[1])
+        if len(dostawa) == 0:
+            break
+    if len(dostawa) == 0:
+        break
+    ile +=1
+tab = []
+for i in range(len(samochody)):
+    df = pd.DataFrame(data=trasy[i])
+    tab.append(df)
+#print(trasa)
+for i, t in enumerate(tab):
+    fig.add_scatter(x=t['x'], y=t['y'], name=f'samochod_{i+1}')
+
 fig.show()
